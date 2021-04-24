2468f802400349c67c8f21f1b13be10f52e163f6
Moulham Hallak <mou990@gmail.com>
2021-04-24 00:58:59 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", theDomHasLoaded, false);
+function theDomHasLoaded(e) {
+document.querySelector("p").innerHTML = "This is really cool!";
+}
