7d26ce71357260b53b59ae917dfb2908918253b5
DKay-code <derya.kaygisiz@outlook.com>
2021-04-27 19:50:00 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", e => {
+    const newText = document.querySelector("#text");
+    newText.innerHTML = "This is really cool!"
+  });
+   
+  
